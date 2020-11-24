90a6cca1960c7aed3e69a136394d87379febcfd4
ImSejin <hackeres123@gmail.com>
2020-11-24 15:39:31 +0900

    ✨ Add: lecture.10

--- sample/20201124.py
+++ sample/20201124.py
@@ -24,12 +24,11 @@ def pay_toll(toll, budget):
         return -1
 
 
-_budget = 1000
+_budget = 200
 _toll = 15
 _budget = pay_toll(_toll, _budget)
 while _budget != -1:
     _budget = pay_toll(_toll, _budget)
-
 
 # 9. continue 문과 break 문을 배워보자
 
@@ -41,3 +40,18 @@ for i in range(0, 10):
         print(f'Odd: {i}')
     else:
         print(f'Even: {i}')
+
+# 10. for 문을 배워보자
+
+for word in '재밌는 파이썬 프로그래밍':
+    print(f'word: {word}')
+
+# 0부터 숫자 10개
+for n in range(10):
+    print('number:', n)
+# 3부터 시작하되 숫자 4가 되면 종료
+for n in range(3, 4):
+    print('number:', n)
+# 1부터 시작해서 10까지, 숫자는 2씩 증가
+for n in range(1, 11, 2):
+    print('number:', n)
