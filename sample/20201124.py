1cfe057b17bee84c9e580b5fab78f834e70930db
ImSejin <hackeres123@gmail.com>
2020-11-24 14:22:38 +0900

    ✨ Add: lecture.8

--- sample/20201124.py
+++ sample/20201124.py
@@ -7,3 +7,25 @@ elif fee == 0:
     print('무료로 통행할 수 있습니다')
 else:
     print('올바르지 않은 통행료입니다')
+
+
+# 8. 제어문 while 문을 배워보자
+
+def pay_toll(toll, budget):
+    if toll == 0:
+        print('무료로 통행하였습니다.')
+        return budget
+
+    if 0 < toll <= budget:
+        print(f'지불 통행료: ${toll}, 현재 예산: ${budget}')
+        return budget - toll
+    else:
+        print(f'예산 부족: ${budget} => you need to have more ${toll - budget}')
+        return -1
+
+
+_budget = 1000
+_toll = 15
+_budget = pay_toll(_toll, _budget)
+while _budget != -1:
+    _budget = pay_toll(_toll, _budget)
