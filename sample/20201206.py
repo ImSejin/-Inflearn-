4f55239d4f8e5d3d63aede9310a57ad4e252f528
ImSejin <hackeres123@gmail.com>
2020-12-08 16:21:04 +0900

    ⚡️ Update: lecture.18

--- sample/20201206.py
+++ sample/20201206.py
@@ -4,6 +4,7 @@
 
 
 class BookReader:
+    # 모든 인스턴스들이 같은 값으로 초기화되는 변수(클래스 변수)
     name = ''
 
     def read_book(self):
@@ -13,12 +14,16 @@ class BookReader:
 reader = BookReader()
 reader.name = '세진'
 reader.read_book()
+print(BookReader().name)
+
 
 # ---
 
 
 class NewBookReader:
+    # '__init__'은 생성자
     def __init__(self, name):
+        # 각 인스턴스가 고유하게 가지는 변수(인스턴스 변수)
         self.name = name
 
     def read_book(self):
